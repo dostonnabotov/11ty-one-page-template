f627a56f1be7f3ef4ef43e3926be59f2b82d3e12
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-03-30 21:45:19 +0300

    eleventy-added

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    const { DateTime } = require("luxon");
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
