f557f603ae2c8bea9ec3bfb4f5ee79dea1dee493
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-04-03 20:56:58 +0300

    layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/css');
+    // eleventyConfig.addPassthroughCopy('./src/css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
 
