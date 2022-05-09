41f7549b71b3d43ae6d50d57208952f16de96ba3
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-05-09 16:12:36 +0300

    11ty added

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,18 @@
-
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('./src/style.css');
-    // eleventyConfig.addPassthroughCopy('./src/css');
-    eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-    const { DateTime } = require("luxon");
+  const { DateTime } = require("luxon");
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    });
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    }
-}
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
