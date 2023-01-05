2b7485efca1aaa91b06541c88103fa3029561930
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2023-01-06 00:15:34 +0200

    bug fixes and improvements

--- .eleventy.js
+++ .eleventy.js
@@ -16,14 +16,14 @@ module.exports = function (eleventyConfig) {
   function sortByOrder(values) {
     let vals = [...values];
     return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
-}
+  }
 
   eleventyConfig.addFilter("sortByOrder", sortByOrder);
 
   return {
     dir: {
       input: "src",
-      output: "public",
+      output: "dist",
     },
   };
 };
